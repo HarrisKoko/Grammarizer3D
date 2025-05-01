bebb0d5fbf41a3c34ffef3b8940029592cee922b
HarrisKoko <hdkokkinakos@gmail.com>
2025-05-01 18:20:27 -0400

    writing index of shader to facelabel

--- helloMaya/helloMaya/LoadMesh.h
+++ helloMaya/helloMaya/LoadMesh.h
@@ -5,6 +5,21 @@
 #include <maya/MArgList.h>
 #include "graph.h"
 
+#include <maya/MFnMesh.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MSelectionList.h>
+#include <maya/MGlobal.h>
+#include <maya/MPointArray.h>
+#include <maya/MIntArray.h>
+#include <maya/MDagPath.h>
+#include <iostream>
+#include <maya/MObjectArray.h>          
+#include <maya/MPlug.h>                 
+#include <maya/MPlugArray.h>            
+#include <maya/MFnDependencyNode.h> 
+#include <maya/MObjectHandle.h>
+
+
 class LoadMeshCmd : public MPxCommand {
 public:
     static void* creator();
@@ -12,6 +27,8 @@ public:
 
     static Graph& getLoadedGraph();
     static bool hasLoadedGraph(); 
+    MObjectArray shaders;
+    MIntArray shaderIndices;
 
 private:
     static Graph s_loadedGraph;
